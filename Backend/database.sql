2c8d08b27ade9dcc24fdb4fa8bcce33f0bf19fc4
wyattmog <145526766+wyattmog@users.noreply.github.com>
2024-10-15 23:02:19 -0500

    Initial commit

---
+++ Backend/database.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE instagram_tool;
+USE instagram_tool;
+
+CREATE TABLE followers (
+    user_id VARCHAR(255) NOT NULL,
+    follower_id VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id, follower_id)
+);
+
+CREATE TABLE following (
+    user_id VARCHAR(255) NOT NULL,
+    following_id VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id, following_id)
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
